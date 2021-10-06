e60e410e9f6bf0aa827fb79165072ccf8b68f89e
michal.przepiora@hoefliger.de <michal.przepiora@hoefliger.de>
2021-10-06 12:08:54 +0200

    Initial commit

---
+++ src/main/java/pl/com/przepiora/parkiva/ParkivaApplication.java
@@ -0,0 +1,13 @@
+package pl.com.przepiora.parkiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/przepiora/parkiva/ParkivaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.przepiora.parkiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
